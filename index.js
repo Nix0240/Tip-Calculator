cc754e12cea41a682e6a5f177e9c50efe2538ee9
Nix0240 <nikhilesh0240@gmail.com>
2022-12-11 09:11:11 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const amount = document.getElementById('amount');
+const guests = document.getElementById('guests');
+const quality = document.getElementById('quality');
+const tipAmount = document.getElementById('tipAmount');
+
+calculate = () => { 
+  const tip = ((amount.value * quality.value) / (guests.value).toFixed(2));
+  amount.value = '';
+  guests.value = '';
+  quality.value = '';
+  if (tip === 'Nan') {
+    tipAmount.innerHTML = 'Tip 0 each';
+    showTipAmount();
+  }
+  else { 
+    tipAmount.innerHTML = 'Tip $' + tip + 'each';
+    showTipAmount();
+  }
+}
+
+showTipAmount = () => { 
+
+  var x = tipAmount;
+  x.classname = 'show';
+  setTimeout(function () { x.className = x.className.replace('show', '') }, 3000);
+
+}
